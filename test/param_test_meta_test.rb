d9efc75cf0b1984bc64577110312003715825d00
Nik Haldimann <nhaldimann@gmail.com>
2013-01-14 18:09:12 -0500

    More meta tests

--- test/param_test_meta_test.rb
+++ test/param_test_meta_test.rb
@@ -7,18 +7,41 @@ class ParamTestMetaTest < ActiveSupport::TestCase
     assert_equal expected_count, instance_methods.grep(/^test/).size
   end
 
-  test "empty parameters list adds no tests" do
-    class SampleTest < ActiveSupport::TestCase
-      param_test "adds no tests", [] do |param| end
-    end
-    assert_test_method_count SampleTest, 0
+  def assert_has_test_method(clazz, expected_method_name)
+    assert_include clazz.instance_methods, expected_method_name
   end
 
-  test "single parameter adds one test" do
-    class SampleTest < ActiveSupport::TestCase
-      param_test "adds one tests %s", [42] do |param| end
+  test "empty parameters adds no tests" do
+    test_case = Class.new ActiveSupport::TestCase do
+      param_test "adds no tests", [] do |param| end
     end
-    assert_test_method_count SampleTest, 1
+    assert_test_method_count test_case, 0
+  end
+
+  test "single parameter list adds one test" do
+    test_case = Class.new ActiveSupport::TestCase do
+      param_test "adds one test %s", [42] do |param| end
+    end
+    assert_test_method_count test_case, 1
+    assert_has_test_method test_case, :test_adds_one_test_42
+  end
+
+  test "multiple parameters generate correct method name" do
+    test_case = Class.new ActiveSupport::TestCase do
+      param_test "adds one test %s two params %s", [[41, 42]] do |first, second| end
+    end
+    assert_test_method_count test_case, 1
+    assert_has_test_method test_case, :test_adds_one_test_41_two_params_42
+  end
+
+  test "two parameter lists generate two methods" do
+    test_case = Class.new ActiveSupport::TestCase do
+      param_test "with two params %s %s",
+        [[41, 42], [43, 44]] do |first, second| end
+    end
+    assert_test_method_count test_case, 2
+    assert_has_test_method test_case, :test_with_two_params_41_42
+    assert_has_test_method test_case, :test_with_two_params_43_44
   end
 
 end

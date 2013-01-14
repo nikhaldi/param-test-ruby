3d13bead0b84f348ee446be7cbb88b4ad5d00795
Nik Haldimann <nhaldimann@gmail.com>
2013-01-14 17:30:45 -0500

    Basic implementation and tests

--- lib/active_support/testing/parametrized.rb
+++ lib/active_support/testing/parametrized.rb
@@ -2,8 +2,14 @@ module ActiveSupport
   module Testing
     module Parametrized
 
-      def param_test
-        # TODO
+      # TODO doc
+      def param_test(description_template, parameters, &block)
+        parameters.each do |param_set|
+          description = description_template % param_set
+          test description do
+            instance_exec param_set, &block
+          end
+        end
       end
 
     end
--- test/param_test_test.rb
+++ test/param_test_test.rb
@@ -2,8 +2,13 @@ require 'helper'
 
 class ParamTestTest < ActiveSupport::TestCase
 
-  def test_foo
-    assert true
+  param_test "single parameter: %s", [1, 2, 3] do |param|
+    assert param.is_a? Numeric
+  end
+
+  param_test "multiple parameters: %s %s",
+  [[1, 1], [2, 2], [3, 3]] do |first_param, second_param|
+    assert_equal first_param, second_param
   end
 
 end
